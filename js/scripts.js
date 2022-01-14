616fef6f17c959c909f25a79b04c63411a58ac70
João Barreto <joao.barreto@vortal.biz>
2022-01-14 19:31:55 +0000

    Translations V1

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,149 @@
-/*!
-* Start Bootstrap - Business Frontpage v5.0.7 (https://startbootstrap.com/template/business-frontpage)
-* Copyright 2013-2021 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-business-frontpage/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+
+
+function translateFunction(value) {
+
+    if (value == "PT") 
+    {
+        document.getElementById('header1').innerHTML = TranslatePT.header1;
+        document.getElementById('header2').innerHTML = TranslatePT.header2;
+        document.getElementById('header3').innerHTML = TranslatePT.header3;
+        document.getElementById('about1').innerHTML = TranslatePT.about1;
+        document.getElementById('about2').innerHTML = TranslatePT.about2;
+        document.getElementById('about3').innerHTML = TranslatePT.about3;
+        document.getElementById('service1').innerHTML = TranslatePT.service1;
+        document.getElementById('service2').innerHTML = TranslatePT.service2;
+        document.getElementById('service3').innerHTML = TranslatePT.service3;
+        document.getElementById('service4').innerHTML = TranslatePT.service4;
+        document.getElementById('service5').innerHTML = TranslatePT.service5;
+        document.getElementById('service6').innerHTML = TranslatePT.service6;
+        document.getElementById('service7').innerHTML = TranslatePT.service7;
+        document.getElementById('service8').innerHTML = TranslatePT.service8;
+        document.getElementById('service9').innerHTML = TranslatePT.service9;
+        document.getElementById('service10').innerHTML = TranslatePT.service10;
+        document.getElementById('service11').innerHTML = TranslatePT.service11;
+        document.getElementById('service12').innerHTML = TranslatePT.service12;
+        document.getElementById('service13').innerHTML = TranslatePT.service13;
+        document.getElementById('service14').innerHTML = TranslatePT.service14;
+        document.getElementById('assisted1').innerHTML = TranslatePT.assisted1;
+        document.getElementById('assisted2').innerHTML = TranslatePT.assisted2;         
+        document.getElementById('community1').innerHTML = TranslatePT.community1;
+        document.getElementById('community2').innerHTML = TranslatePT.community2;       
+        document.getElementById('payments1').innerHTML = TranslatePT.payments1;
+        document.getElementById('payments2').innerHTML = TranslatePT.payments2;
+        document.getElementById('contact1').innerHTML = TranslatePT.contact1;
+        document.getElementById('contact2').innerHTML = TranslatePT.contact2;
+        document.getElementById('contactform1').innerHTML = TranslatePT.contactform1;
+        document.getElementById('contactform2').innerHTML = TranslatePT.contactform2;
+        document.getElementById('contactform3').innerHTML = TranslatePT.contactform3;
+        document.getElementById('contactform4').value = TranslatePT.contactform4;        
+        document.getElementById('footer').innerHTML = TranslatePT.footer;
+    }
+    if (value == "EN") 
+    {
+        document.getElementById('header1').innerHTML = TranslateEN.header1;
+        document.getElementById('header2').innerHTML = TranslateEN.header2;
+        document.getElementById('header3').innerHTML = TranslateEN.header3;
+        document.getElementById('about1').innerHTML = TranslateEN.about1;
+        document.getElementById('about2').innerHTML = TranslateEN.about2;
+        document.getElementById('about3').innerHTML = TranslateEN.about3;
+        document.getElementById('service1').innerHTML = TranslateEN.service1;
+        document.getElementById('service2').innerHTML = TranslateEN.service2;
+        document.getElementById('service3').innerHTML = TranslateEN.service3;
+        document.getElementById('service4').innerHTML = TranslateEN.service4;
+        document.getElementById('service5').innerHTML = TranslateEN.service5;
+        document.getElementById('service6').innerHTML = TranslateEN.service6;
+        document.getElementById('service7').innerHTML = TranslateEN.service7;
+        document.getElementById('service8').innerHTML = TranslateEN.service8;
+        document.getElementById('service9').innerHTML = TranslateEN.service9;
+        document.getElementById('service10').innerHTML = TranslateEN.service10;
+        document.getElementById('service11').innerHTML = TranslateEN.service11;
+        document.getElementById('service12').innerHTML = TranslateEN.service12;
+        document.getElementById('service13').innerHTML = TranslateEN.service13;
+        document.getElementById('service14').innerHTML = TranslateEN.service14;
+        document.getElementById('assisted1').innerHTML = TranslateEN.assisted1;
+        document.getElementById('assisted2').innerHTML = TranslateEN.assisted2;         
+        document.getElementById('community1').innerHTML = TranslateEN.community1;
+        document.getElementById('community2').innerHTML = TranslateEN.community2;       
+        document.getElementById('payments1').innerHTML = TranslateEN.payments1;
+        document.getElementById('payments2').innerHTML = TranslateEN.payments2;
+        document.getElementById('contact1').innerHTML = TranslateEN.contact1;
+        document.getElementById('contact2').innerHTML = TranslateEN.contact2;
+        document.getElementById('contactform1').innerHTML = TranslateEN.contactform1;
+        document.getElementById('contactform2').innerHTML = TranslateEN.contactform2;
+        document.getElementById('contactform3').innerHTML = TranslateEN.contactform3;
+        document.getElementById('contactform4').value = TranslateEN.contactform4;        
+        document.getElementById('footer').innerHTML = TranslateEN.footer;
+    }
+    if (value == "DE") 
+    {
+        document.getElementById('header1').innerHTML = TranslateDE.header1;
+        document.getElementById('header2').innerHTML = TranslateDE.header2;
+        document.getElementById('header3').innerHTML = TranslateDE.header3;
+        document.getElementById('about1').innerHTML = TranslateDE.about1;
+        document.getElementById('about2').innerHTML = TranslateDE.about2;
+        document.getElementById('about3').innerHTML = TranslateDE.about3;
+        document.getElementById('service1').innerHTML = TranslateDE.service1;
+        document.getElementById('service2').innerHTML = TranslateDE.service2;
+        document.getElementById('service3').innerHTML = TranslateDE.service3;
+        document.getElementById('service4').innerHTML = TranslateDE.service4;
+        document.getElementById('service5').innerHTML = TranslateDE.service5;
+        document.getElementById('service6').innerHTML = TranslateDE.service6;
+        document.getElementById('service7').innerHTML = TranslateDE.service7;
+        document.getElementById('service8').innerHTML = TranslateDE.service8;
+        document.getElementById('service9').innerHTML = TranslateDE.service9;
+        document.getElementById('service10').innerHTML = TranslateDE.service10;
+        document.getElementById('service11').innerHTML = TranslateDE.service11;
+        document.getElementById('service12').innerHTML = TranslateDE.service12;
+        document.getElementById('service13').innerHTML = TranslateDE.service13;
+        document.getElementById('service14').innerHTML = TranslateDE.service14;
+        document.getElementById('assisted1').innerHTML = TranslateDE.assisted1;
+        document.getElementById('assisted2').innerHTML = TranslateDE.assisted2;         
+        document.getElementById('community1').innerHTML = TranslateDE.community1;
+        document.getElementById('community2').innerHTML = TranslateDE.community2;       
+        document.getElementById('payments1').innerHTML = TranslateDE.payments1;
+        document.getElementById('payments2').innerHTML = TranslateDE.payments2;
+        document.getElementById('contact1').innerHTML = TranslateDE.contact1;
+        document.getElementById('contact2').innerHTML = TranslateDE.contact2;
+        document.getElementById('contactform1').innerHTML = TranslateDE.contactform1;
+        document.getElementById('contactform2').innerHTML = TranslateDE.contactform2;
+        document.getElementById('contactform3').innerHTML = TranslateDE.contactform3;
+        document.getElementById('contactform4').value = TranslateDE.contactform4;        
+        document.getElementById('footer').innerHTML = TranslateDE.footer;
+    }
+    if (value == "CN") 
+    {
+        document.getElementById('header1').innerHTML = TranslateCN.header1;
+        document.getElementById('header2').innerHTML = TranslateCN.header2;
+        document.getElementById('header3').innerHTML = TranslateCN.header3;
+        document.getElementById('about1').innerHTML = TranslateCN.about1;
+        document.getElementById('about2').innerHTML = TranslateCN.about2;
+        document.getElementById('about3').innerHTML = TranslateCN.about3;
+        document.getElementById('service1').innerHTML = TranslateCN.service1;
+        document.getElementById('service2').innerHTML = TranslateCN.service2;
+        document.getElementById('service3').innerHTML = TranslateCN.service3;
+        document.getElementById('service4').innerHTML = TranslateCN.service4;
+        document.getElementById('service5').innerHTML = TranslateCN.service5;
+        document.getElementById('service6').innerHTML = TranslateCN.service6;
+        document.getElementById('service7').innerHTML = TranslateCN.service7;
+        document.getElementById('service8').innerHTML = TranslateCN.service8;
+        document.getElementById('service9').innerHTML = TranslateCN.service9;
+        document.getElementById('service10').innerHTML = TranslateCN.service10;
+        document.getElementById('service11').innerHTML = TranslateCN.service11;
+        document.getElementById('service12').innerHTML = TranslateCN.service12;
+        document.getElementById('service13').innerHTML = TranslateCN.service13;
+        document.getElementById('service14').innerHTML = TranslateCN.service14;
+        document.getElementById('assisted1').innerHTML = TranslateCN.assisted1;
+        document.getElementById('assisted2').innerHTML = TranslateCN.assisted2;         
+        document.getElementById('community1').innerHTML = TranslateCN.community1;
+        document.getElementById('community2').innerHTML = TranslateCN.community2;       
+        document.getElementById('payments1').innerHTML = TranslateCN.payments1;
+        document.getElementById('payments2').innerHTML = TranslateCN.payments2;
+        document.getElementById('contact1').innerHTML = TranslateCN.contact1;
+        document.getElementById('contact2').innerHTML = TranslateCN.contact2;
+        document.getElementById('contactform1').innerHTML = TranslateCN.contactform1;
+        document.getElementById('contactform2').innerHTML = TranslateCN.contactform2;
+        document.getElementById('contactform3').innerHTML = TranslateCN.contactform3;
+        document.getElementById('contactform4').value = TranslateCN.contactform4;        
+        document.getElementById('footer').innerHTML = TranslateCN.footer;
+    }
+}
